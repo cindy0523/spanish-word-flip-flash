f05d8496abb32476b27447b9eb0dfb8c6af010d4
Thu <minhthuphan2002@gmail.com>
2026-03-21 14:13:20 +0700

    edit worker to 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 2,
+  workers: 2,
   reporter: [
     ['list'],
     ['junit', { outputFile: 'reports-e2e/junit.xml' }],
